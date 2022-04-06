e2712c42185ef2c6f27d6f1219b8bd6937935378
Pavel Gordienko <guzmo6@rambler.ru>
2022-04-06 11:06:17 +0300

    invoke Unity Events work

--- Assets/Scripts/HeroInputReader.cs
+++ Assets/Scripts/HeroInputReader.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HeroInputReader : MonoBehaviour
 {
 
     [SerializeField] private Hero _hero;
     // Update is called once per frame
-    private void Update()
+    /*private void Update()
     {
         var horizontal = Input.GetAxis("Horizontal");
         _hero.SetDirection(horizontal);
         
-        /*if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             _hero.SetDirection(-1);
         }
@@ -23,6 +24,12 @@ public class HeroInputReader : MonoBehaviour
         else
         {
             _hero.SetDirection(0);
-        }*/
+        }
+    }*/
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context )
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
     }
 }
